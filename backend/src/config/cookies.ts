39ffd85af45570a1d550142451329f5807002c1d
aryanxengineer <aryanxengineer@gmail.com>
2026-05-31 23:13:30 +0530

    changed dthe value of sameSite as none

--- backend/src/config/cookies.ts
+++ backend/src/config/cookies.ts
@@ -1,13 +1,15 @@
+import { env } from "./dotenv.js";
+
 export const accessTokenCookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax" as const,
+    secure: env.NODE_ENV === "production",
+    sameSite: "none" as const,
     maxAge: 24 * 60 * 60 * 1000 // 1 day
 };
 
 export const refreshTokenCookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax" as const,
+    secure: env.NODE_ENV === "production",
+    sameSite: "none" as const,
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
 };
